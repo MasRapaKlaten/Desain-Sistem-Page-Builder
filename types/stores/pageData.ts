df4a2158b6429e7ea6118c15819ce21f84b3f53a
Rapa <acountutama1010@gmail.com>
2026-08-04 09:54:27 +0700

    Change arrays to readonly arrays in Page interfaces

--- types/stores/pageData.ts
+++ types/stores/pageData.ts
@@ -1,21 +1,24 @@
 interface Page {
   id: string;
-  rootComponentIds: Array<string>;
-  componentIds: Array<string>;
+  rootComponentIds: ReadonlyArray<string>;
+  componentIds: ReadonlyArray<string>;
 }
-interface PageDataState {
+export interface PageDataState {
   totalPages: number;
-  pageIds: Array<string>;
-  pagesById: Record<string, Page>;
-  componentsById: Record<string, NodeComponent>;
+  pageIds: ReadonlyArray<string>;
+  pagesById: Readonly<Record<string, Page | undefined>>;
+  componentsById: Readonly<Record<string, ComponentNode | undefined>>;
 }
 interface PageDataAction {
   addPage: (pageId: string) => void;
   deletePage: (pageId: string) => void;
-  addComponent: (componentData: NodeComponent) => void;
+  addComponent: (payload: {id: string, pageId: string, type: ComponentType}) => void;
   deleteComponent: (componentId: string) => void;
-  updateComponentAttribute: <T extends keyof ComponentAttribute, K extends keyof ComponentAttribute[T]>(payload: {componentId: string, type: T, key: K, value: ComponentAttribute[T][K]}) => void;
+  updateComponentAttribute: <T extends keyof MappingAttribute, K extends keyof MappingAttribute[T]>(payload: {componentId: string, type: T, key: K, value: MappingAttribute[T][K]}) => void;
   resetComponentAttribute: (payload: {componentId: string, type: ComponentType}) => void;
-  resetComponentKeyAttribute: <T extends keyof ComponentAttribute>(payload: {componentId: string, type: T, key: keyof ComponentAttribute[T]}) => void;
+  resetComponentKeyAttribute: <T extends keyof MappingAttribute>(payload: {componentId: string, type: T, key: keyof MappingAttribute[T]}) => void;
+  linkComponent: (payload: {pageId: string, parentId: ParentId, childId: string}) => void;
+  unlinkComponent: (payload: {pageId: string, parentId: ParentId, childId: string}) => void;
+  updateDepth: (payload: {pageId: string, componentId: string}) => void;
 }
 type PageDataStore = PageDataState & PageDataAction;
