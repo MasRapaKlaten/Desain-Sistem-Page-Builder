75fcb3036f1dd97f0ff93d5209ed634dd4cc6200
Rapa <acountutama1010@gmail.com>
2026-08-05 09:06:34 +0700

    Update pageData.ts

--- types/stores/pageData.ts
+++ types/stores/pageData.ts
@@ -1,13 +1,13 @@
 interface Page {
   id: string;
-  rootComponentIds: ReadonlyArray<string>;
-  componentIds: ReadonlyArray<string>;
+  rootComponentIds: Array<string>;
+  componentIds: Array<string>;
 }
 export interface PageDataState {
   totalPages: number;
-  pageIds: ReadonlyArray<string>;
-  pagesById: Readonly<Record<string, Page | undefined>>;
-  componentsById: Readonly<Record<string, ComponentNode | undefined>>;
+  pageIds: Array<string>;
+  pagesById: Record<string, Page | undefined>;
+  componentsById: Record<string, ComponentNode | undefined>;
 }
 interface PageDataAction {
   addPage: (pageId: string) => void;
@@ -16,9 +16,9 @@ interface PageDataAction {
   deleteComponent: (componentId: string) => void;
   updateComponentAttribute: <T extends keyof MappingAttribute, K extends keyof MappingAttribute[T]>(payload: {componentId: string, type: T, key: K, value: MappingAttribute[T][K]}) => void;
   resetComponentAttribute: (payload: {componentId: string, type: ComponentType}) => void;
-  resetComponentKeyAttribute: <T extends keyof MappingAttribute>(payload: {componentId: string, type: T, key: keyof MappingAttribute[T]}) => void;
-  linkComponent: (payload: {pageId: string, parentId: ParentId, childId: string}) => void;
-  unlinkComponent: (payload: {pageId: string, parentId: ParentId, childId: string}) => void;
+  resetComponentKeyAttribute: <T extends keyof MappingAttribute>(payload: {componentId: string, type: T, attribute: keyof MappingAttribute[T]}) => void;
+  linkComponent: (payload: {pageId: string, parentId: string, childId: string}) => void;
+  unlinkComponent: (payload: {pageId: string, parentId: string, childId: string}) => void;
   updateDepth: (payload: {pageId: string, componentId: string}) => void;
 }
-type PageDataStore = PageDataState & PageDataAction;
+type PageDataStore = Readonly<PageDataState> & PageDataAction;
